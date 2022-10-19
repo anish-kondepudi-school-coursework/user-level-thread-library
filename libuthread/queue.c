7686b90b0a690096fa02d6e6a8b19266dc21e75b
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-10-19 16:43:42 -0700

    Implement queue_create

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -5,12 +5,19 @@
 #include "queue.h"
 
 struct queue {
-	/* TODO Phase 1 */
+	unsigned front, end, size, capacity;
+	int* array;
 };
 
 queue_t queue_create(void)
 {
-	/* TODO Phase 1 */
+	queue_t queue = (queue_t) malloc(sizeof(struct queue));
+
+	queue->array = (int*) malloc(queue->capacity * sizeof(int));
+	queue->front = queue->end = 0;
+	queue->capacity = queue->size = 1;
+
+	return queue;
 }
 
 int queue_destroy(queue_t queue)
@@ -42,4 +49,3 @@ int queue_length(queue_t queue)
 {
 	/* TODO Phase 1 */
 }
-
