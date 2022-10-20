ae6639ddeaa3cd0c6a80c7b02d477e27d7982984
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-10-20 10:51:00 -0700

    Create test for invalid inputs to queue_destroy

--- apps/queue_tester.c
+++ apps/queue_tester.c
@@ -18,7 +18,7 @@ do {									\
 /* Create */
 void test_create_queue(void)
 {
-	fprintf(stderr, "*** TEST create ***\n");
+	fprintf(stderr, "*** TEST queue_create ***\n");
 
 	TEST_ASSERT(queue_create() != NULL);
 }
@@ -26,21 +26,31 @@ void test_create_queue(void)
 /* Enqueue/Dequeue simple */
 void test_simple_enqueue_dequeue(void)
 {
+	fprintf(stderr, "*** TEST test_simple_enqueue_dequeue ***\n");
+
 	int data = 3, *ptr;
-	queue_t q;
-
-	fprintf(stderr, "*** TEST queue_simple ***\n");
-
-	q = queue_create();
+	queue_t q = queue_create();
 	queue_enqueue(q, &data);
 	queue_dequeue(q, (void**)&ptr);
 	TEST_ASSERT(ptr == &data);
+}
+
+// check invalid arguments
+// check edge cases with lot of arguments when queue completely full
+// check if free queue works
+
+void test_invalid_inputs_queue_destroy()
+{
+	fprintf(stderr, "*** TEST test_invalid_inputs_queue_destroy ***\n");
+
+	TEST_ASSERT(queue_destroy(NULL) == -1);
 }
 
 int main(void)
 {
 	test_create_queue();
 	test_simple_enqueue_dequeue();
+	test_invalid_inputs_queue_destroy();
 
 	return 0;
 }
