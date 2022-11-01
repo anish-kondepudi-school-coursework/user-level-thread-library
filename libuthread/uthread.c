562ce2bfc5e1704804c04abf57279ae005517f3c
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-11-01 14:57:06 -0700

    Add states and idle thread to uthread

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -10,27 +10,34 @@
 #include "uthread.h"
 #include "queue.h"
 
-uthread_ctx_t* g_main_ctx;
-uthread_ctx_t* g_current_ctx;
-queue_t g_queue;
+enum State {
+	Idle,
+	Ready,
+	Blocked,
+	Running
+};
 
 typedef struct uthread_tcb* uthread_tcb_t;
 
 struct uthread_tcb {
 	uthread_ctx_t ctx;
+	enum State state;
 	void* stack;
 };
 
+uthread_tcb_t g_current_tcb;
+queue_t g_queue;
+
 static void iterator_delete_tcb(queue_t queue, void* data) {
 	uthread_tcb_t tcb = (uthread_tcb_t) data;
-	if (&tcb->ctx == g_current_ctx) {
+	if (&tcb->ctx == &g_current_tcb->ctx) {
 		assert(queue_delete(queue, data) == 0);
 		uthread_ctx_destroy_stack(tcb->stack);
 		free(tcb);
 	}
 }
 
-uthread_tcb_t create_tcb(uthread_func_t func, void* arg) {
+uthread_tcb_t create_and_enqueue_tcb(uthread_func_t func, void* arg) {
 	// Create TCB
 	uthread_tcb_t tcb = (uthread_tcb_t) malloc(sizeof(struct uthread_tcb));
 	if (tcb == NULL) {
@@ -58,12 +65,26 @@ uthread_tcb_t create_tcb(uthread_func_t func, void* arg) {
 		return NULL;
 	}
 
+	// Set TCB state to ready
+	tcb->state = Ready;
 	return tcb;
 }
 
-void switch_context(uthread_ctx_t* previous_ctx, uthread_ctx_t* new_ctx) {
-	g_current_ctx = new_ctx;
-	uthread_ctx_switch(previous_ctx, new_ctx);
+void switch_context(uthread_tcb_t previous_tcb, uthread_tcb_t new_tcb) {
+	// Update states for TCBs
+	if (previous_tcb->state == Running) {
+		previous_tcb->state = Ready;
+	}
+
+	if (new_tcb->state == Ready) {
+		new_tcb->state = Running;
+	}
+
+	// Update global current TCB
+	g_current_tcb = new_tcb;
+
+	// Switch context
+	uthread_ctx_switch(&previous_tcb->ctx, &new_tcb->ctx);
 }
 
 void uthread_yield(void) {
@@ -73,27 +94,19 @@ void uthread_yield(void) {
 	assert(queue_enqueue(g_queue, (void*) tcb) == 0);
 
 	// Switch context to execute next thread
-	switch_context(g_current_ctx, &tcb->ctx);
+	switch_context(g_current_tcb, tcb);
 }
 
 void uthread_exit(void) {
 	// Delete current threads TCB from queue
-	uthread_ctx_t current_copy_ref = *g_current_ctx;
+	assert(queue_length(g_queue) >= 2);
 	queue_iterate(g_queue, iterator_delete_tcb);
-
-	// If more threads in queue, yield to next queue
-	if (queue_length(g_queue) > 0) {
-		uthread_yield();
-		return;
-	}
-
-	// Switch context to main thread (since all threads are done)
-	switch_context(&current_copy_ref, g_main_ctx);
+	uthread_yield();
 }
 
 int uthread_create(uthread_func_t func, void* arg) {
 	// Create TCB
-	uthread_tcb_t tcb = create_tcb(func, arg);
+	uthread_tcb_t tcb = create_and_enqueue_tcb(func, arg);
 	if (tcb == NULL) {
 		return -1;
 	}
@@ -103,27 +116,38 @@ int uthread_create(uthread_func_t func, void* arg) {
 
 int uthread_start(uthread_func_t func, void* arg) {
 	// Initialize global data members if needed
-	bool global_data_members_uninitialized =
-		g_queue == NULL &&
-		g_main_ctx == NULL &&
-		g_current_ctx == NULL;
-	if (global_data_members_uninitialized) {
+	if (g_queue == NULL && g_current_tcb == NULL) {
 		g_queue = queue_create();
-		g_main_ctx = (uthread_ctx_t*) malloc(sizeof(uthread_ctx_t));
-		g_current_ctx = (uthread_ctx_t*) malloc(sizeof(uthread_ctx_t));
+		g_current_tcb = (uthread_tcb_t) malloc(sizeof(struct uthread_tcb));
 
-		if (g_queue == NULL || g_main_ctx == NULL || g_current_ctx == NULL) {
+		if (g_queue == NULL || g_current_tcb == NULL) {
 			return -1;
 		}
 	}
 
-	// Create TCB
-	uthread_tcb_t tcb = create_tcb(func, arg);
+	// Create and Enqueue next TCB
+	uthread_tcb_t tcb = create_and_enqueue_tcb(func, arg);
 	if (tcb == NULL) {
 		return -1;
 	}
-	// Switch context to execute newly created thread
-	switch_context(g_main_ctx, &tcb->ctx);
+
+	// Create TCB for idle thread
+	uthread_tcb_t idle_thread_tcb = (uthread_tcb_t) malloc(sizeof(struct uthread_tcb));
+	if (idle_thread_tcb == NULL) {
+		return -1;
+	}
+	idle_thread_tcb->state = Idle;
+
+	// Enqueue TCB for idle thread and update global TCB
+	g_current_tcb = idle_thread_tcb;
+	if (queue_enqueue(g_queue, idle_thread_tcb) == -1) {
+		return -1;
+	}
+
+	// Yield on idle thread until it's only process left in queue
+	while (queue_length(g_queue) > 1) {
+		uthread_yield();
+	}
 
 	return 0;
 }
