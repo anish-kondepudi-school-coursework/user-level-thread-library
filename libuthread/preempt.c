78f1406c90aec4c627f93569b5b23483d443ed5e
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-11-02 13:20:34 -0700

    Implement alarm timer to raise signal

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 #include <sys/time.h>
 
 #include "private.h"
@@ -14,9 +16,25 @@
  * 100Hz is 100 times per second
  */
 #define HZ 100
+#define MILLI_TO_MICRO_CONVERSION_CONSTANT 1000
+
+struct sigaction sa;
+
+int set_alarm_timer(void) {
+	struct itimerval value;
+
+	value.it_interval.tv_sec = 0;
+    value.it_interval.tv_usec = 0;
+
+	value.it_value.tv_sec = 0;
+    value.it_value.tv_usec = HZ * MILLI_TO_MICRO_CONVERSION_CONSTANT;
+
+	setitimer(ITIMER_REAL, &value, NULL);
+}
 
 void alarm_handler(int signum) {
 	printf("\nBeep, beep, beep, we in the alarmmmmm!\n");
+	set_alarm_timer();
 }
 
 void preempt_disable(void) {
@@ -24,7 +42,11 @@ void preempt_disable(void) {
 }
 
 void preempt_enable(void) {
-	/* TODO Phase 4 */
+	sa.sa_handler = alarm_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGALRM, &sa, NULL);
+	set_alarm_timer();
 }
 
 void preempt_start(bool preempt) {
