2315a1461eb884b4730a4126299b5a7138464e5e
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-11-02 01:01:08 -0700

    Implemented spin locks

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "queue.h"
 #include "sem.h"
@@ -8,21 +9,22 @@
 struct semaphore {
 	queue_t queue;
 	size_t count;
-	int lock;
+	volatile int lock;
 };
 
-void spinlock_lock(int *lock)
-{
-	while (test_and_set(lock) == 1);
+void spinlock_lock(volatile int* lock) {
+	while (__sync_lock_test_and_set(lock, 1)) {
+        // Do nothing. This GCC builtin instruction
+        // ensures memory barrier.
+    }
 }
 
-void spinlock_unlock(int *lock)
-{
-	*lock = 0;
+void spinlock_unlock(volatile int* lock) {
+	__sync_synchronize(); // Memory barrier.
+    *lock = 0;
 }
 
-sem_t sem_create(size_t count)
-{
+sem_t sem_create(size_t count) {
 	sem_t sem = (sem_t) malloc(sizeof(struct semaphore));
 	if (sem == NULL) {
 		return NULL;
@@ -38,8 +40,7 @@ sem_t sem_create(size_t count)
 	return sem;
 }
 
-int sem_destroy(sem_t sem)
-{
+int sem_destroy(sem_t sem) {
 	if (sem == NULL || queue_length(sem->queue) != 0) {
 		return -1;
 	}
@@ -52,25 +53,25 @@ int sem_destroy(sem_t sem)
 	return 0;
 }
 
-int sem_down(sem_t sem)
-{
+int sem_down(sem_t sem) {
 	if (sem == NULL) {
 		return -1;
 	}
 
-	spinlock_lock(sem->lock);
+	/* spinlock_lock(&sem->lock); */
+	struct uthread_tcb* current_tcb = uthread_current();
+	queue_enqueue(sem->queue, current_tcb);
 	while (sem->count == 0) {
 		uthread_block();
 	}
 
 	sem->count--;
-	spinlock_unlock(sem->lock);
+	/* spinlock_unlock(&sem->lock); */
 
 	return 0;
 }
 
-int sem_up(sem_t sem)
-{
-	/* TODO Phase 3 */
+int sem_up(sem_t sem) {
+
 }
 
